53ec429f983e585564b0e34320ee28464d56e8b4
TomBenRu <mail@thomas-ruff.de>
2022-12-15 16:36:00 +0100

    day 11

--- day_15/main.py
+++ day_15/main.py
@@ -11,7 +11,7 @@ all_sensors_and_beacons = {p for pair in data for p in pair}
 no_beacons = set()
 # curr_row = 10
 curr_row = 2_000_000
-max_xy__distress_signal = 4_000_000
+max_xy__distress_signal = 4_000_000  # test_input: 20
 
 not_poss_in_row = set()
 
@@ -25,12 +25,13 @@ for (s_x, s_y), (b_x, b_y) in data:
 print('part 1:', len(not_poss_in_row-all_sensors_and_beacons))
 
 for row in range(max_xy__distress_signal + 1):
+    if not row % 100_000:
+        print(f'check row {row}')
     x_coords = [(0, max_xy__distress_signal+1)]
     for (s_x, s_y), (b_x, b_y) in data:
         manh_dist = abs(s_x - b_x) + abs(s_y - b_y)
         if (dist_in_curr_row := (manh_dist - abs(s_y - row))) < 0:
             continue
-
         x_low, x_high = max(0, s_x - dist_in_curr_row), s_x + dist_in_curr_row + 1
         new_x_coords = []
         for part_low, part_high in x_coords:
